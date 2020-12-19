ef962899db5031b6c5c5fb1bb1777ec30336297e
Kevin <kevinhill@live.com>
2020-12-19 13:54:41 -0800

    updated to use es6 syntax

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ const express = require ('express');
 const bodyParser = require ('body-parser');
 const app = express ();
 
-app.listen (3000, function () {
+app.listen (3000, () => {
   console.log ('application listenting on http://localhost:3000');
 });
 
-app.get ('/', function (req, res) {
+app.use (bodyParser.urlencoded ({extended: true}));
+
+app.get ('/', (req, res) => {
   res.sendFile (__dirname + '/index.html');
+});
+
+app.post ('/jokes', (req, res) => {
+  console.log (req.body);
 });
