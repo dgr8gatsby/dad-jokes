fb7f5a9255cdb2e339c87e9677ad66f8542cfcaa
Kevin <kevinhill@live.com>
2020-12-20 10:15:11 -0800

    Adding comments to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const bodyParser = require ('body-parser');
 const MongoClient = require ('mongodb').MongoClient;
 
+// TODO: Heather you can use local Mongo instead for now
 MongoClient.connect ('mongodb-connection-string', (err, client) => {});
 const app = express ();
 
@@ -9,12 +10,19 @@ app.listen (3000, () => {
   console.log ('application listenting on http://localhost:3000');
 });
 
+// Used to parse POST body data
 app.use (bodyParser.urlencoded ({extended: true}));
 
+// Default path for main application
 app.get ('/', (req, res) => {
   res.sendFile (__dirname + '/index.html');
 });
 
+// Handle new jokes
 app.post ('/jokes', (req, res) => {
+  // Just loging requests from the client in the console
   console.log (req.body);
+  // TODO: Save in a Database
 });
+
+// TODO: Make a path to return a test joke
