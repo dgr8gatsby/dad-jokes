d196a185541429033d067b882249c56a4d717ef8
Heather Empson <hrempson@gmail.com>
2020-12-20 11:00:12 -0800

    app listening

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ const MongoClient = require ('mongodb').MongoClient;
 
 MongoClient.connect ('mongodb-connection-string', (err, client) => {});
 const app = express ();
+const PORT = process.env.PORT || 3000;
+const hostname = 'localhost'
 
-app.listen (3000, () => {
-  console.log ('application listenting on http://localhost:3000');
+app.listen (PORT, (err) => {
+  if(err) throw err;
+  console.log (`application listenting on http://${hostname}: ${PORT}`);
 });
 
 app.use (bodyParser.urlencoded ({extended: true}));
