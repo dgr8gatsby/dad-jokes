1e8e778da7864efadc87f1da07c30625c06ecf73
Kevin <kevinhill@live.com>
2020-12-20 19:39:43 -0800

    adding app-link, updating app-header

--- client/components/app-header/app-header.js
+++ client/components/app-header/app-header.js
@@ -34,15 +34,8 @@ class AppHeader extends HTMLElement {
         padding: 20px 10px;
         }
 
-        .header a {
-        float: left;
-        color: black;
-        text-align: center;
-        padding: 12px;
-        text-decoration: none;
-        font-size: 18px; 
-        line-height: 25px;
-        border-radius: 4px;
+        .header app-link {
+        
         }
 
         .header a.logo {
@@ -76,13 +69,13 @@ class AppHeader extends HTMLElement {
         }
         }
     </style>
-    
+
     <div class="header">
     <a href="#default" class="logo">CompanyLogo</a>
     <div class="header-right">
-        <a class="active" href="#home">Home</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <slot name="navigation">
+        </slot>
+        <a href="#test">TEST</a>
     </div>
     </div>
         `;
