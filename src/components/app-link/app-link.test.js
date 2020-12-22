aa029c2f593983dabaee768b04fb01101a8a5ae3
Kevin <kevinhill@live.com>
2020-12-21 21:20:26 -0800

    fixed test

---
+++ src/components/app-link/app-link.test.js
@@ -0,0 +1,24 @@
+const AppLink = require ('./app-link');
+
+describe ('app-link', () => {
+  it ('creates a navigation link for the app', () => {
+    // Create a new instance of app-link
+    const element = document.createElement ('app-link', {
+      is: AppLink,
+    });
+
+    // Set attributes for element
+    element.setAttribute ('link-name', 'Home');
+    element.setAttribute ('link-route', 'home');
+
+    // Add custom element to the DOM
+    document.body.appendChild (element);
+
+    //Query for the element with
+    const appLinkElement = document.body.querySelector ('app-link');
+
+    return Promise.resolve ().then (() => {
+      expect (appLinkElement.name).toBe ('Home');
+    });
+  });
+});
