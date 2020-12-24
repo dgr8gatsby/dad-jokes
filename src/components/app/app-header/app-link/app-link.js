45d8cfe21652376fea45a38c4e4d295727c4e3d8
Kevin Hill <kevinhill@live.com>
2020-12-24 12:17:27 -0800

    Kevin client (#6)
    * updating app-link
    
    * move comps 2 src, this is how it is supposed 2 B
    
    * updating components to check registration
    
    * fixed test
    
    * fixed branching logic in app-link for test
    
    * Fixed test again
    
    * navigate support for app-link & app-header
    
    * removing event handler from template in app-link
    
    * Adding some SVGs and tracking attributions
    
    * adding initial screens
    
    * Added jest-esm-transformer to support es6 modules for jest test
    
    * Adding support for dynamically loading components
    
    * Hooked up navigate event to swap screens.
    
    * Added basic Add joke screen removed app-screen
    
    * Added meta description for SEO
    
    * fixed joke rendering to use div's instead of headers for accessibility.
    
    * removed parcel replaced with rollup
    
    * added node server start to commands
    
    * clean up project
    
    * cleaning up
    
    * test: added test for app-link click
    
    * added expects to test

---
+++ src/components/app/app-header/app-link/app-link.js
@@ -0,0 +1,80 @@
+// This class will be the container for the entire dad joke application
+
+const ELEMENT_NAME = 'app-link';
+
+export default class AppLink extends HTMLElement {
+  constructor () {
+    super ();
+    this.handleClick = this.handleClick.bind (this);
+    var shadow = this.attachShadow ({
+      mode: 'open',
+    });
+  }
+
+  handleClick (e) {
+    e.preventDefault ();
+    const event = new CustomEvent ('navigate', {
+      detail: this.route,
+      composed: true,
+      bubbles: true,
+    });
+    this.dispatchEvent (event);
+  }
+
+  static get observedAttributes () {
+    return ['link-name', 'link-route'];
+  }
+
+  get name () {
+    return this.getAttribute ('link-name');
+  }
+
+  get route () {
+    return this.getAttribute ('link-route');
+  }
+
+  attributeChangedCallback (attribute, oldValue, newValue) {
+    this.render ();
+  }
+
+  connectedCallback () {
+    this.addEventListener ('click', this.handleClick, false);
+    this.render ();
+  }
+
+  render () {
+    this.shadowRoot.innerHTML = `
+        <style>
+          a {     
+            float: left;
+            color: black;
+            text-align: center;
+            padding: 12px;
+            text-decoration: none;
+            font-size: 18px; 
+            line-height: 25px;
+            border-radius: 4px;
+          }
+          @media screen and (max-width: 500px) {
+            a {
+                float: none;
+                display: block;
+                text-align: left;
+            }
+          }
+          a:hover {
+            background-color: #ddd;
+            color: black;
+          }
+          a.active {
+            background-color: dodgerblue;
+            color: white;
+          }
+    
+        </style>
+        <a href="#${this.route}">${this.name}</a>
+      `;
+  }
+}
+
+customElements.define (ELEMENT_NAME, AppLink);
---
+++ src/components/app/app.js
@@ -0,0 +1,68 @@
+import AppHeader from './app-header/app-header.js';
+
+// This class will be the container for the entire dad joke application
+const ELEMENT_NAME = 'dad-joke-app';
+const DEFAULT_SCREEN = 'view-joke';
+
+class App extends HTMLElement {
+  constructor () {
+    super ();
+    this.handleNavigate = this.handleNavigate.bind (this);
+    var shadow = this.attachShadow ({
+      mode: 'open',
+    });
+
+    this.AppHeaderComponent = AppHeader;
+  }
+
+  screen = null;
+
+  connectedCallback () {
+    this.shadowRoot.addEventListener ('navigate', this.handleNavigate);
+    this.render ();
+    this.loadScreen ();
+  }
+
+  handleNavigate (e) {
+    this.loadScreen (e.detail);
+  }
+
+  // Dynamically load screens
+  async loadScreen (name = DEFAULT_SCREEN /* Default screen */) {
+    // Check if the screen has already been loaded
+    if (customElements.get (name) != undefined) {
+      // This doesn't deal with an out of date screen that has already loaded
+      this.displayScreen (name);
+    } else {
+      try {
+        let screenPath = `./screens/${name}/${name}.js`;
+        const screenConstructor = await import (screenPath);
+        this.screen = screenConstructor;
+        this.displayScreen (name);
+      } catch (e) {
+        throw new CustomError (
+          `Unable to load module`,
+          `${name}.js might not exist, this is the ${screenPath}`
+        );
+      }
+    }
+  }
+
+  displayScreen (name) {
+    const screenElement = this.shadowRoot.querySelector ("p[name='screens']");
+    screenElement.innerHTML = '';
+    screenElement.innerHTML = `<${name}></${name}>`;
+  }
+
+  render () {
+    this.shadowRoot.innerHTML = `
+        <h1>Dad Jokes</h1>
+        <p>
+            <slot name="header"></slot>
+            <p name="screens"></p>
+        </p>
+      `;
+  }
+}
+
+customElements.define (ELEMENT_NAME, App);
